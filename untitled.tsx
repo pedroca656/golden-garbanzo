d1ba3040b6a2c68e5ca1832f4f68c12966f5b073
pedroca656 <pedro.ivo.fraga@hotmail.com>
2018-06-18 18:37:16 -0300

    Commit com o mapa feito, falta todo resto do trabalho.

---
+++ untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="40" tileheight="40" tilecount="450" columns="30">
+ <image source="tileset.png" width="1200" height="639"/>
+</tileset>
